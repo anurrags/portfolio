d05ba5c63a9e6df88772b009c41e57e494056b8c
Anurag Sharma <iamanurag0501@gmail.com>
2024-07-19 00:15:59 +0530

    netlify form changes

--- src/components/contact-form/Contactform.tsx
+++ src/components/contact-form/Contactform.tsx
@@ -1,5 +1,25 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./contactform.css";
-const Contactform = () => {
+
+const Contactform: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const myForm = event.currentTarget;
+    const formData = new FormData(myForm);
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData.toString()).toString(),
+    })
+      .then(() => navigate("/thank-you/"))
+      .catch((error) => alert(error));
+  };
+
   return (
     <div className="contact-form-container">
       <form
@@ -7,33 +27,40 @@ const Contactform = () => {
         className="contact-form"
         data-netlify="true"
         method="POST"
+        onSubmit={handleSubmit}
       >
+        {/* Hidden input for form name */}
+        <input type="hidden" name="form-name" value="contact" />
+
+        {/* Form fields */}
         <input
           className="contact-form-input"
-          type="name"
+          type="text"
           name="name"
           placeholder="Your Name"
+          required
         />
         <input
           className="contact-form-input"
           type="email"
           name="email"
-          id=""
           placeholder="E-mail"
+          required
         />
         <input
           className="contact-form-input"
-          type="subject"
+          type="text"
+          name="subject"
           placeholder="Write a Subject"
         />
         <textarea
           className="contact-form-input"
-          name="Message"
-          id=""
+          name="message"
           cols={30}
           rows={10}
           placeholder="Your Message"
-        />
+          required
+        ></textarea>
         <button className="contact-form-btn" type="submit">
           Submit
         </button>
