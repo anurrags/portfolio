a219f987031989870173aafca1ee866fcf61d089
Anurag Sharma <iamanurag0501@gmail.com>
2026-03-22 15:18:18 +0530

    chore: strictly resolve all peer dependencies and purge legacy components

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,6 @@ export { default as Hero } from "./hero";
 export { default as Overview } from "./overview";
 export { default as Card } from "./card";
 export { default as Experience } from "./experience";
-export { default as Timeline } from "./timeline";
 export { default as Skill } from "./skill";
 export { default as Contact } from "./contact";
 export { default as ScrollButton } from "./scroll-button/ScrollButton";
